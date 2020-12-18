4c1307e9dc2ee8bf21e56bbc30ab8c883751e92f
owiekris <owiechris@gmail.com>
2020-12-18 23:42:37 +0000

    Display functions for parking lot created

---
+++ printing.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "datamanagement.h"
+
+char* DisplayDriverProfile(DriverProfile* profile);
+char* DisplayParkingProfile(ParkingProfile* profile);
+
+void DisplayAllDriverProfileData(DriverProfile* first_profile);
+void DisplayAllParkingProfileData(ParkingProfile* first_profile);
